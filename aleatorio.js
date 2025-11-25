c560f6a2cd07538d768481394bbdae7084751cfe
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2025-11-25 12:12:25 -0300

    ergeg

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Laura", "Igor", "Maria", "Eduardo", "Nathili", "Emerson", "Andreia", "Zelia"];
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
